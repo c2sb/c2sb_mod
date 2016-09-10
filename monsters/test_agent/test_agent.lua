6f3c4b3e2440b3fdd4a1c73aee390b13afc615db
Adam Heinermann <aheinerm@gmail.com>
2016-09-10 13:12:09 -0700

    Fixed some activateable behaviours

--- monsters/test_agent/test_agent.lua
+++ monsters/test_agent/test_agent.lua
@@ -125,7 +125,7 @@ function checkActivate()
   if self.interacted then
     self.interacted = false
 
-    local canActivate1 = (self.caos.behaviors & CAOS.PERMISSIONS.ACTIVATE_1) ~= 0
+    local canActivate1 = (self.caos.behaviors & CAOS.PERMISSIONS.ACTIVATE_1) ~= 0 or (self.caos.attributes & CAOS.ATTRIBUTES.ACTIVATEABLE) ~= 0
     local canActivate2 = (self.caos.behaviors & CAOS.PERMISSIONS.ACTIVATE_2) ~= 0
 
     -- Randomly choose an activate event
@@ -134,9 +134,9 @@ function checkActivate()
       event = {CAOS.EVENT.ACTIVATE_1, CAOS.EVENT.ACTIVATE_2}
       event = event[self.random:randu32() % 2 + 1]
     elseif canActivate1 then
-      event = CAOS.EVENT.ACTIVATE_2
-    elseif canActivate2 then
       event = CAOS.EVENT.ACTIVATE_1
+    elseif canActivate2 then
+      event = CAOS.EVENT.ACTIVATE_2
     else
       return false
     end
