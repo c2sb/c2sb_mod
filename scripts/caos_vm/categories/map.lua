9ee71703cdac4839b0300b5ba47baaf3b0071a8b
Adam Heinermann <aheinerm@gmail.com>
2016-09-10 12:56:59 -0700

    Stubbed GRAP

--- scripts/caos_vm/categories/map.lua
+++ scripts/caos_vm/categories/map.lua
@@ -7,7 +7,15 @@
 down = CAOS.DIRECTIONS.DOWN
 
 -- Target now constantly emits an amount of a CA into the room it is in.
+-- TODO
 CAOS.TargCmd("emit")
+
+-- Returns the room id at point x,y on the map. If the point is outside the room system,
+-- it returns -1.
+-- TODO
+CAOS.TargCmd("grap", function(x, y)
+  return -1
+end)
 
 -- Returns the value of the left constant.
 left = CAOS.DIRECTIONS.LEFT
@@ -28,10 +36,12 @@ end)
 right = CAOS.DIRECTIONS.RIGHT
 
 -- Returns the id of the room containing the midpoint of the specified agent.
+-- TODO
 CAOS.Cmd("room")
 
 -- Sets the type of a room. The meaning of the types depends on the game. RATE also uses the room
 -- type.
+-- TODO
 CAOS.Cmd("rtyp")
 
 -- Returns the value of the up constant.
